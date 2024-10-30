5411e770bbfc9914201bdf1880541fed6b310909
Diana <114948133+Dinasi4ka@users.noreply.github.com>
2024-10-30 22:00:24 +0200

    text alignment fixed

--- bll/classes/base_acii_art_generator.py
+++ bll/classes/base_acii_art_generator.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from bll.validation import validate_text_input, validate_number_input, validate_color_choice,validate_font_choice, validate_symbol
 
 class BaseAsciiArt(ABC):  
     def __init__(self):
@@ -24,13 +25,13 @@ class BaseAsciiArt(ABC):
         colors = self.get_available_colors()
         print("Доступні кольори:", colors)
         chosen_color = input("Виберіть колір з наведених: ")
-        return chosen_color if chosen_color in colors else "white"
+        return validate_color_choice(chosen_color, colors)
     
     def choose_font(self):
         fonts = self.get_available_fonts()
         print("Доступні шрифти:", fonts)
         chosen_font = input("Виберіть шрифт з наведених: ")
-        return chosen_font if chosen_font in fonts else "standard"
+        return validate_font_choice(chosen_font, fonts)
 
     def get_available_fonts(self):
         return ['tty', 'banner', 'rozzo', 'standard', 'slant', 'big', 'doom', 'alligator', 'digital', 'cybermedium']
@@ -39,10 +40,10 @@ class BaseAsciiArt(ABC):
         return ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
     def get_art_parameters(self):
-        user_input = input("Введіть слово або фразу для ASCII-арту: ")
-        chosen_symbol = input("Введіть символ для ASCII-арту (за замовчуванням '#'): ") or "#"
-        width_factor = int(input("Введіть масштаб по ширині (ціле число, 1 для нормального розміру): "))
-        height_factor = int(input("Введіть масштаб по висоті (ціле число, 1 для нормального розміру): "))
+        user_input = validate_text_input(input("Введіть слово або фразу для ASCII-арту: "))
+        chosen_symbol = validate_symbol(input("Введіть символ для ASCII-арту (за замовчуванням '#'): ") or "#")
+        width_factor = int(validate_number_input(input("Введіть масштаб по ширині (ціле число, 1 для нормального розміру): "), min_value=1))
+        height_factor = int(validate_number_input(input("Введіть масштаб по висоті (ціле число, 1 для нормального розміру): "), min_value=1))
         return user_input, chosen_symbol, width_factor, height_factor
 
     def save_art(self, colored_art):
--- bll/classes/my_acii_art.py
+++ bll/classes/my_acii_art.py
@@ -7,45 +7,42 @@ class MyAciiArt(BaseAsciiArt):
         super().__init__()
         self.alphabet = ABC123dictionary  
 
-    def generate_art(self, text, symbol, width_factor, height_factor):
+    def generate_art(self, text, symbol, width_factor, height_factor, alignment='center'):
         art_lines = ['' for _ in range(5 * height_factor)]
+        
         for char in text.upper():
             char_art = self.alphabet.get(char, self.alphabet[' '])
             for i, line in enumerate(char_art):
                 art_lines[i] += line.replace('#', symbol) * width_factor
                 art_lines[i] += ' ' * width_factor 
 
-        return self.scale_ascii_art('\n'.join(art_lines), width_factor, height_factor)
+        console_width = 80
+        
+        if alignment == "center":
+            aligned_art = [line.center(console_width) for line in art_lines]
+        elif alignment == "right":
+            aligned_art = [line.rjust(console_width) for line in art_lines]
+        else: 
+            aligned_art = [line.ljust(console_width) for line in art_lines]
 
-    def align_text(self, art, alignment):
-        max_length = max(len(line) for line in art.splitlines())
-        aligned_art = []
-        for line in art.splitlines():
-            if alignment == 'left':
-                aligned_art.append(line.ljust(max_length))
-            elif alignment == 'center':
-                aligned_art.append(line.center(max_length))
-            elif alignment == 'right':
-                aligned_art.append(line.rjust(max_length))
-            else:
-                aligned_art.append(line)  
         return '\n'.join(aligned_art)
-    
+
     def choose_alignment(self):
-        print("Виберіть вирівнювання (left, center, right):")
-        alignment = input("Ваш вибір: ")
-        return alignment if alignment in ['left', 'center', 'right'] else 'left'
+        while True:
+            alignment = input("Виберіть вирівнювання (left, center, right): ").strip().lower()
+            if alignment in ["left", "center", "right"]:
+                return alignment
+            else:
+                print("Недопустиме значення. Будь ласка, виберіть left, center або right.")
 
     def run(self):
         user_input, chosen_symbol, width_factor, height_factor = self.get_art_parameters()
         chosen_color = self.choose_color() 
-        chosen_font = self.choose_font()  
+        chosen_font = self.choose_font()
         alignment = self.choose_alignment()  
 
-        ascii_art = self.generate_art(user_input, chosen_symbol, width_factor, height_factor)
-        aligned_art = self.align_text(ascii_art, alignment)
-
-        colored_art = colored(aligned_art, chosen_color)
+        ascii_art = self.generate_art(user_input, chosen_symbol, width_factor, height_factor, alignment)
+        colored_art = colored(ascii_art, chosen_color)  
         print(colored_art)
 
         self.save_art(colored_art)
