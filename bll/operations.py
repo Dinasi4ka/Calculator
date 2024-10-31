03eeb1398a63ee59ac0aa50e01ad532d6a3e1c84
Diana <114948133+Dinasi4ka@users.noreply.github.com>
2024-11-01 00:27:26 +0200

    use some patterns

--- bll/operations.py
+++ bll/operations.py
@@ -1,27 +1,40 @@
 import math
+from abc import ABC, abstractmethod
 
-def addition(operand1, operand2):
-    return operand1 + operand2
+class Operation(ABC):
+    @abstractmethod
+    def execute(self, operand1, operand2=None):
+        pass
 
-def subtraction(operand1, operand2):
-    return operand1 - operand2
+class Addition(Operation):
+    def execute(self, operand1, operand2):
+        return operand1 + operand2
 
-def  multiplication(operand1, operand2):
-    return operand1 * operand2
+class Subtraction(Operation):
+    def execute(self, operand1, operand2):
+        return operand1 - operand2
 
-def division(operand1, operand2):
-    if operand2 == 0:
-        raise ZeroDivisionError("Ділення на нуль неможливе!")
-    return operand1 / operand2
+class Multiplication(Operation):
+    def execute(self, operand1, operand2):
+        return operand1 * operand2
 
-def  power(operand1, operand2):
-    return pow(operand1,operand2)
+class Division(Operation):
+    def execute(self, operand1, operand2):
+        if operand2 == 0:
+            raise ZeroDivisionError("Ділення на нуль неможливе!")
+        return operand1 / operand2
 
-def  square_root(operand1):
-    if operand1 >= 0:
-        return math.sqrt(operand1)
-    else:
-        return "Неможливо обчислити квадратний корінь із від'ємного числа"
+class Power(Operation):
+    def execute(self, operand1, operand2):
+        return pow(operand1, operand2)
 
-def  find_remainder(operand1, operand2):
-    return operand1 % operand2
+class SquareRoot(Operation):
+    def execute(self, operand1):
+        if operand1 >= 0:
+            return math.sqrt(operand1)
+        else:
+            return "Неможливо обчислити квадратний корінь із від'ємного числа"
+
+class FindRemainder(Operation):
+    def execute(self, operand1, operand2):
+        return operand1 % operand2
